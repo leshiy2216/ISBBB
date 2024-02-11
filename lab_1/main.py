a83b41bbcda391650492c10270a741fc26badfdf
leshiy2216 <cyberstanislaw2004@gmail.com>
2024-02-11 12:22:56 +0300

    added files

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,16 +1,24 @@
 alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 
 
-step = int(input('Step: '))
-message = input('Message for schipher: ').upper()
-finish =  ''
+def encryption():
+    f = open('unencrypted.txt', 'r', encoding='utf-8')
+    content = f.read()
+    content_upper = content.upper()
+    finish =  ''
+    step = int(input('Step: '))
+    for i in content_upper:
+        idx = alphabet.find(i)
+        idx2 = (idx + step) % len(alphabet)
+        if i in alphabet:
+            finish += alphabet[idx2]
+        else:
+            finish += i
+    with open('encrypted.txt', 'w', encoding='utf-8') as d:
+        d.write(finish)
+    with open('key.txt', 'w', encoding='utf-8') as e:
+        e.write(str(step))
 
 
-for i in message:
-    idx = alphabet.find(i)
-    idx2 = idx + step
-    if i in alphabet:
-        finish += alphabet[idx2]
-    else:
-        finish += i
-print(finish)
+if __name__ == "__main__":
+    encryption()
