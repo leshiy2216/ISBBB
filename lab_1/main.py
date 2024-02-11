03783161d1b761d94095ef226bd8f09608f45558
leshiy2216 <cyberstanislaw2004@gmail.com>
2024-02-11 12:22:04 +0300

    1task completed

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,2 +1,16 @@
-a = "hell"
-print(a)
+alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+
+
+step = int(input('Step: '))
+message = input('Message for schipher: ').upper()
+finish =  ''
+
+
+for i in message:
+    idx = alphabet.find(i)
+    idx2 = idx + step
+    if i in alphabet:
+        finish += alphabet[idx2]
+    else:
+        finish += i
+print(finish)
