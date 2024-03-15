9a0b21435daac900044fe67dd691f1ae859c6e47
leshiy2216 <smbeybulatov@gmail.com>
2024-03-15 16:33:32 +0400

    frequency is ready

--- lab_1/frequency.py
+++ lab_1/frequency.py
@@ -1,16 +1,31 @@
-def frequency():
-    f = open('cod3.txt', 'r', encoding='utf-8')
-    content = f.read()
-    content_upper = content.upper()
+import collections
 
-    
-    freq = {}
-    total = len(content_upper)
-    for symbol in content_upper:
-        if symbol in freq:
-            freq[symbol] += 1
-        else:
-            freq[symbol] = 1
-    for symbol, count in sorted(freq.items()):
-        freq = count / total
-        print(f"simvol: {symbol}, freq: {freq:.4f}")
+
+def frequency():
+    f = open('cod3.txt', 'r', encoding='UTF-8')
+    text = f.read()
+    text = text.upper()
+    leng = len(text)
+    alphabetcrypt = dict()
+    for i in text:
+        alphabetcrypt[i] = text.count(i) / leng
+    alphabetcrypt = dict(sorted(alphabetcrypt.items()))
+    print(alphabetcrypt)
+    replace_symbol(text, alphabetcrypt)
+
+def replace_symbol(text, alphabetcrypt):
+    alphabet = {1: 'О',
+                2: 'И',
+                3: 'Е',
+                4: 'Т',
+                5: 'А',
+                6: 'С',
+                7: 'Н',
+                8: 'В',
+                9: 'Р',
+                10: 'Л',
+                11: 'М',
+                12: 'К',
+                13: 'Д'}
+
+frequency()
