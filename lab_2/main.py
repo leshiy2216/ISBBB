e86bd816bba281fd9515cfca2a71ba336d44fe1f
leshiy2216 <smbeybulatov@gmail.com>
2024-05-18 15:30:56 +0400

    added 3rd test and func for read file

--- lab_2/main.py
+++ lab_2/main.py
@@ -1,6 +1,9 @@
 import json
 import math
+
 from scipy.special import gammaincc
+
+PI_VALUES = [0.2148, 0.3672, 0.2305, 0.1875]
 
 
 # first test
@@ -27,3 +30,30 @@ def block_frequency_test(sequence, block_size=8):
     chi_squared = 4 * block_size * sum((pi - 0.5)**2 for pi in proportions)
     p_value = gammaincc(num_blocks / 2, chi_squared / 2)
     return p_value
+
+
+# third test
+def longest_run_of_ones_test(sequence):
+    n = len(sequence)
+    max_run = 0
+    cur_run = 0
+    for bit in sequence:
+        if bit == '1':
+            cur_run += 1
+            max_run = max(max_run, cur_run)
+        else:
+            cur_run = 0
+    if max_run <= 25:
+        v = 0
+        for k in range(max_run + 1):
+            v += (PI_VALUES[k] * (n - k))
+        p_value = math.erfc(abs(v - 0.5) / (math.sqrt(2) * 0.5))
+    else:
+        p_value = 0.0
+    return p_value
+
+
+def read_bit_sequences_from_json(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
